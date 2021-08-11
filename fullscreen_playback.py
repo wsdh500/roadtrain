48fd184b19f20e5514763488071ed4db67b55ef6
Sam Hart <wsdh500@york.ac.uk>
2021-08-11 16:34:19 +0100

    Use the inverted videos as the secondary video source.

--- fullscreen_playback.py
+++ fullscreen_playback.py
@@ -8,7 +8,10 @@
 from video import video_playback
 
 dir = 'videos/test_patterns/double/'
-videos = (dir+'double_display_0.mp4',dir+'double_display_1.mp4',)
+videos = (dir+'double_display_0.mp4',
+          dir+'double_display_1.mp4',
+          dir+'inverted_double_display_0.mp4',
+          dir+'inverted_double_display_1.mp4',)
 fps = 30
 fullscreen = True
 video_playback(videos,fps,fullscreen)
