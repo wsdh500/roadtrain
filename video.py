3d47c122981d7ee98e8bddc53e86acdffa382a2c
Sam Hart <wsdh500@york.ac.uk>
2021-08-11 11:24:09 +0100

    Separate out stream and window initialisation.

--- video.py
+++ video.py
@@ -3,40 +3,48 @@ import cv2
 
 from display import n_displays , x_offset , y_offset
 
+from sys import exit
+
 QUIT_KEY = ord('q')
 
-def setup_window(file_path,window_name,x_offset,y_offset,full_screen):
-    cap = cv2.VideoCapture(file_path)
+caps = []
+windows = []
+n = n_displays()
+
+def open_stream(stream):
+    cap = cv2.VideoCapture(stream)
     if(cap.isOpened() == False):
-        print('Cannot open',file_path)
-        return None , None
-    video_window = window_name
+        print('Cannot open stream',stream)
+        sys.exit()
+    return cap
+
+def open_window(window_name,x_offset,y_offset,full_screen):
     if full_screen:
-        cv2.namedWindow(video_window,cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty(video_window,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.namedWindow(window_name,cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty(window_name,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
     else:
-        cv2.namedWindow(video_window,cv2.WINDOW_AUTOSIZE)
-    cv2.moveWindow(video_window,x_offset,y_offset)
+        cv2.namedWindow(window_name,cv2.WINDOW_AUTOSIZE)
+    cv2.moveWindow(window_name,x_offset,y_offset)
+    return window_name
+
+def setup_window(path,window_name,x_offset,y_offset,full_screen):
+    cap = open_stream(path)
+    video_window = open_window(window_name,x_offset,y_offset,full_screen)
     return cap , video_window
 
 def video_playback(paths,fps,full_screen):
-    caps = []
-    windows = []
-    n = n_displays()
-    for i in range(n):
-        path = paths[i % len(paths)]
-        name = "Display:"+str(i)
-        cap , window = setup_window(path,name,x_offset(i),y_offset(i),full_screen)
-        if(cap == None or window == None):
-            print('Cannot open window',str(i))
-            return
+    for path in paths:
+        cap = open_stream(path)
         caps.append(cap)
+    for i in range(n):
+        name = "Display:"+str(i)
+        window = open_window(name,x_offset(i),y_offset(i),full_screen)
         windows.append(window)
     try:
         RUNNING = True
         while RUNNING:
             for i in range(n):
-                cap = caps[i]
+                cap = caps[i % len(caps)]
                 window = windows[i]
                 ret , frame = cap.read()
                 if ret == True:
