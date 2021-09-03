72003790514248df87efde29ed533878078b40b5
Sam Hart <wsdh500@york.ac.uk>
2021-09-03 11:11:59 +0100

    Mock imports so that it will generate on readthedocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+autodoc_mock_imports=['vlc','cv2','pyrealsense2','Xlib','RPi']
+
 highlight_language = 'python'
 
 
